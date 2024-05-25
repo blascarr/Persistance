6e7b0fc44fabc3ac405a54347336fc31ad1b9ac6
blascarr <adrian.blascarr@gmail.com>
2024-05-25 10:05:23 +0200

    Central Filesystem Storage Class

--- Persistance.h
+++ Persistance.h
@@ -30,55 +30,30 @@ class IJSONSerializable {
 
 class ISerializable : public IStringSerializable, public IJSONSerializable {};
 
-#if defined(ESP8266)
-#include <LittleFS.h>
-class LittleFS_Storage : public IStorage {
-  public:
-	LittleFS_Storage() {
-		if (!LittleFS.begin()) {
-		}
-	}
-	~LittleFS_Storage() { LittleFS.end(); }
-	void save(const String &data, const String &path) override {
-		File file = LittleFS.open(path, "w");
-		if (!file) {
-			return;
-		}
-		file.println(data);
-		file.close();
-	}
-
-	String load(const String &path) override {
-		File file = LittleFS.open(path, "r");
-		if (!file) {
-			return "";
-		}
-		String data = file.readStringUntil('\n');
-		file.close();
-		return data;
-	}
-	void saveJSON(const JsonDocument &data, const String &path) override {}
-	JsonDocument loadJSON(const String &path) override {
-		JsonDocument doc;
-		return doc;
-	}
-};
+#if !defined(NVS) && defined(SPIFFS_STORAGE)
+#include <FS.h>
+#define FILESYSTEM SPIFFS
+#define FILESYSTEMNAME "SPIFFS"
 #endif
 
-#if defined(ESP32)
-
-#include <FS.h>
+#if !defined(NVS) && !defined(SPIFFS_STORAGE)
 #include <LittleFS.h>
-class LittleFS_Storage : public IStorage {
+#define FILESYSTEM LittleFS
+#define FILESYSTEMNAME "LittleFS"
+#endif
+
+#if !defined(NVS)
+class FS_Storage : public IStorage {
   public:
-	LittleFS_Storage() {
-		if (!LittleFS.begin()) {
+	FS_Storage() {
+		if (!FILESYSTEM.begin()) {
+			Serial.println(FILESYSTEMNAME);
 			Serial.println("LittleFS mount failed");
 		}
 	}
-	~LittleFS_Storage() { LittleFS.end(); }
+	~FS_Storage() { FILESYSTEM.end(); }
 	void save(const String &data, const String &path) override {
-		File file = LittleFS.open(path, "w");
+		File file = FILESYSTEM.open(path, "w");
 		if (!file) {
 			Serial.println("Failed to open file for writing");
 			return;
@@ -88,7 +63,45 @@ class LittleFS_Storage : public IStorage {
 	}
 
 	String load(const String &path) override {
-		File file = LittleFS.open(path, "r");
+		File file = FILESYSTEM.open(path, "r");
+		if (!file) {
+			Serial.println("Failed to open file for reading");
+			return "";
+		}
+		String data = file.readStringUntil('\n');
+		file.close();
+		return data;
+	}
+	void saveJSON(const JsonDocument &data, const String &path) override {}
+	JsonDocument loadJSON(const String &path) override {
+		JsonDocument doc;
+		return doc;
+	}
+};
+#endif
+
+/*
+#if defined(ESP32) && !defined(NVS)
+class FS_Storage : public IStorage {
+  public:
+	FS_Storage() {
+		if (!FILESYSTEM.begin()) {
+			Serial.println("LittleFS mount failed");
+		}
+	}
+	~FS_Storage() { FILESYSTEM.end(); }
+	void save(const String &data, const String &path) override {
+		File file = FILESYSTEM.open(path, "w");
+		if (!file) {
+			Serial.println("Failed to open file for writing");
+			return;
+		}
+		file.println(data);
+		file.close();
+	}
+
+	String load(const String &path) override {
+		File file = FILESYSTEM.open(path, "r");
 		if (!file) {
 			Serial.println("Failed to open file for writing");
 			return "";
@@ -104,18 +117,34 @@ class LittleFS_Storage : public IStorage {
 	}
 };
 #endif
+*/
 
-#if defined(SPIFFS_STORAGE)
-class SPIFFS_Storage : public IStorage {
+#if defined(NVS)
+#include <Preferences.h>
+
+class NVS_Storage : public IStorage {
   public:
-	void save(const String &data, const String &path) override {}
+	void save(const String &data, const String &path) override {
+		preferences.begin("my-app", false);
+		preferences.putString(path, data);
+		preferences.end();
+	}
 
-	String load(const String &path) override { return ""; }
+	String load(const String &path) override {
+		String data = preferences.getString(path);
+		preferences.end();
+		return data;
+	}
+
+	void remove(const String &path) {
+		preferences.remove(path);
+		preferences.end();
+	}
+	void removeAll() {
+		preferences.clear();
+		preferences.end();
+	}
 };
-#endif
-
-#if defined(PREFERENCE_STORAGE)
-
 #endif
 
 class Persistance {
